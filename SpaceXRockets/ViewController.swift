04478726841dd9299841c64b96178282c66a06c0
TheXXI <58274104+TheXXI@users.noreply.github.com>
2022-03-24 15:09:16 +0300

    Initial Commit

---
+++ SpaceXRockets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpaceXRockets
+//
+//  Created by Михаил Курис on 24.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
